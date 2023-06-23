37a87e737ef330eeb3a50958d01380ff98fe9d62
Thiago Gonçalves <mg.az.ul@hotmail.com>
2023-06-23 16:03:54 -0300

    Fix PaymentWithTiket

--- store/src/main/java/com/ecommerce/store/domain/PaymentWithTiket.java
+++ store/src/main/java/com/ecommerce/store/domain/PaymentWithTiket.java
@@ -1,7 +1,10 @@
 package com.ecommerce.store.domain;
 
+import java.time.Instant;
+
 import com.ecommerce.store.domain.enums.PaymentStatus;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,15 +14,23 @@ import lombok.Setter;
 @Entity
 public class PaymentWithTiket extends Payment {
 
-	private Integer installment;
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant dueDate;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant payday;
 
 	public PaymentWithTiket() {
 		super();
 	}
 
-	public PaymentWithTiket(Long id, PaymentStatus status, Order order, Integer installments) {
+	public PaymentWithTiket(Long id, PaymentStatus status, Order order, Instant dueDate, Instant payday) {
 		super(id, status, order);
-		this.installment = installments;
+		this.dueDate = dueDate;
+		this.payday = payday;
 	}
+
+	
 
 }
