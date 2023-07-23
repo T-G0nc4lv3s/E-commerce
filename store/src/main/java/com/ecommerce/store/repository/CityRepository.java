ff5e5eb2b0f4d95861e7c7cc2a7a3f7b782a0adb
Thiago Gonçalves <mg.az.ul@hotmail.com>
2023-07-23 16:54:02 -0300

    CityRepository, findAllCities refactor

--- store/src/main/java/com/ecommerce/store/repository/CityRepository.java
+++ store/src/main/java/com/ecommerce/store/repository/CityRepository.java
@@ -6,15 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.ecommerce.store.domain.City;
-import com.ecommerce.store.projections.CityMinProjection;
 
 public interface CityRepository extends JpaRepository<City, Long> {
-
 	
-	@Query(nativeQuery = true, 
-			value = "SELECT * "
-			+ "FROM tb_city "
-			+ "INNER JOIN tb_state "
-			+ "WHERE tb_city.state_id = tb_state.id")
-	public List<CityMinProjection> findAllCities();
+	@Query("SELECT city "
+			+"FROM City city "
+			+"JOIN FETCH city.state ")
+	public List<City> findAllCities();
 }
